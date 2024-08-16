c5e399b568a89d1c7a26b9ac1f5abfd83c01a89d
MiguelAngelTG7 <michael_angello@hotmail.com>
2024-08-16 11:57:52 -0500

    adjust cell size

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,9 @@ import background from './assets/background.png'
 
 const App = () => {
   return (
-    <div style={{ backgroundImage: `url(${background})` }} className='app'>
+    <div style={{ backgroundImage: `url(${background})`,
+                  backgroundRepeat: "repeat",
+                  backgroundSize: "cover" }} className='app'>
       <Weather/>
     </div>
   )
